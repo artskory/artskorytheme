4f5022abb502eb203ea73300a12a0eb7b0ccdcfd
arstkory <contact@artskory.fr>
2018-06-12 22:24:45 +0200

    Fix 404

--- 404.php
+++ 404.php
@@ -3,52 +3,54 @@
     <head>
         <?php wp_head(); ?>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="language" content="fr">
-        <meta name="copyright" content="//artskory.fr">
-        <meta name="author" content="ArtsKory.fr">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title><?php bloginfo('title'); ?></title>
-        <link href="<?php bloginfo('stylesheet_directory'); ?>/css/style.min.css" rel="stylesheet">
-        <!-- Favicon -->
-        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://www.artskory.fr/apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://www.artskory.fr/apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://www.artskory.fr/apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://www.artskory.fr/apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://www.artskory.fr/apple-touch-icon-60x60.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://www.artskory.fr/apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://www.artskory.fr/apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://www.artskory.fr/apple-touch-icon-152x152.png" />
-        <link rel="icon" type="image/png" href="http://www.artskory.fr/favicon-196x196.png" sizes="196x196" />
-        <link rel="icon" type="image/png" href="http://www.artskory.fr/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/png" href="http://www.artskory.fr/favicon-32x32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="http://www.artskory.fr/favicon-16x16.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="http://www.artskory.fr/favicon-128.png" sizes="128x128" />
-        <meta name="application-name" content="Artskory | Acceuil"/>
-        <meta name="msapplication-TileColor" content="#FFFFFF" />
-        <meta name="msapplication-TileImage" content="http://www.artskory.fr/mstile-144x144.png" />
-        <meta name="msapplication-square70x70logo" content="http://www.artskory.fr/mstile-70x70.png" />
-        <meta name="msapplication-square150x150logo" content="http://www.artskory.fr/mstile-150x150.png" />
-        <meta name="msapplication-wide310x150logo" content="http://www.artskory.fr/mstile-310x150.png" />
-        <meta name="msapplication-square310x310logo" content="http://www.artskory.fr/mstile-310x310.png" />
-        <meta name="p:domain_verify" content="d6574383fd676cc7f60ea9d23db587a9"/>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <!-- CSS theme -->
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/style.min.css">
+        <!-- CSS font-artskory -->
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/font-artskory.min.css">
+        <!-- CSS bootstrap-social -->
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/bootstrap-social.min.css">
+        <!-- CSS responsive -->
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/responsive.min.css">
+        <!-- CSS animate -->
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/animate.min.css" >
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Scroll -->
+        <script src="<?php bloginfo('stylesheet_directory'); ?>/js/scrollToTop.min.js"></script>
+        <!-- Video -->
+        <script src="<?php bloginfo('stylesheet_directory'); ?>/js/random-video.min.js"></script>
+        <!-- Parallax -->
+        <script src="<?php bloginfo('stylesheet_directory'); ?>/js/parallax.js"></script>
     </head>
+
     <body class="body">
-        <svg width="380px" height="500px" version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 240.1 240.1" style="enable-background:new 0 0 222 196.5;" xml:space="preserve">
-            <polygon id="polygon-1" class="st0" fill="#85C555" points="126.6,24.6 113.5,87.4 228.3,195.9 "/>
-            <polygon id="polygon-2" class="st1" fill="#32A8DF" points="113.3,29.2 77.2,166.1 11.7,206.5 "/>
-            <polygon id="polygon-3" class="st2" fill="#F2742E" points="228.3,210.9 187.9,173.6 119.4,165.9 "/>
-            <polygon id="polygon-4" class="st3" fill="#EA465A" points="152.2,189 102.9,165.9 17.6,215.5 "/>
-        </svg>
-        <div class="message-box">
-          <h1>404</h1>
-          <p>ERREUR</p>
-          <p>Page introuvable</p>
-          <div class="buttons-con">
-            <div class="action-link-wrap">
-              <a href="<?php bloginfo( 'url' ) ?>" class="link-button">Revenir à l'accueil</a>
+        <div class="center">
+            <div class="row justify-content-center">
+                <div class="col-xs-6">
+                    <svg width="380px" height="500px" version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 240.1 240.1" style="enable-background:new 0 0 222 196.5;" xml:space="preserve">
+                        <polygon id="polygon-1" class="st0" fill="#85C555" points="126.6,24.6 113.5,87.4 228.3,195.9 "/>
+                        <polygon id="polygon-2" class="st1" fill="#32A8DF" points="113.3,29.2 77.2,166.1 11.7,206.5 "/>
+                        <polygon id="polygon-3" class="st2" fill="#F2742E" points="228.3,210.9 187.9,173.6 119.4,165.9 "/>
+                        <polygon id="polygon-4" class="st3" fill="#EA465A" points="152.2,189 102.9,165.9 17.6,215.5 "/>
+                    </svg>
+                </div>
+                <div class="col-xs-6">
+                    <div class="message-box">
+                      <h1>404</h1>
+                      <p>ERREUR</p>
+                      <p>Page introuvable</p>
+                      <div class="buttons-con">
+                        <div class="action-link-wrap">
+                          <a href="<?php bloginfo( 'url' ) ?>" class="link-button">Revenir à l'accueil</a>
+                        </div>
+                      </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
     </body>
 </html>
